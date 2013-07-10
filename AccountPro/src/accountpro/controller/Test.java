c5f30eb85e8de4468bc0b7e097cf22220b0bd9f9
heyvishy <shukla81@gmail.com>
2013-07-10 16:46:07 -0400

    test

---
+++ AccountPro/src/accountpro/controller/Test.java
@@ -0,0 +1,16 @@
+package accountpro.controller;
+
+public class Test {
+
+	public static void main(String args[]){
+		
+		int[] arr = new int[]{1,2,3,4,5};
+
+	 int x = 2;
+	 String s = String.valueOf(x);
+	 
+	 boolean b = x<5;
+	 System.out.println(b);
+	}
+	
+}
